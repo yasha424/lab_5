01bd689dae2ff39ce30c90463608105642898c94
yasha424 <jakob20022404@gmail.com>
2021-05-15 10:10:48 +0300

    working

--- main.cpp
+++ main.cpp
@@ -7,17 +7,18 @@ int main(int argc, char const *argv[]) {
         cout << "There should be 5 arguments!\n";
         return 0;
     }
-    Rectangle rec(0, 0, 20000, 20000);
+    Rectangle rec(0, 0, 7000, 7000);
     RTree tree(rec);
 
     tree.makeTree(argv[1]);
     Point point(stod(argv[2]), stod(argv[3]));
-    double radius = stod(argv[4]) * 1000;
+    double radius = stod(argv[4]) * 10000;
 
     vector <Point> result = tree.findPoints(point, radius);
 
     for (size_t i = 0; i < result.size(); i++) {
-        cout << result[i].latitude << ", " << result[i].longitude << endl;
+        cout << result[i].latitude << ", " << result[i].longitude << ", " << result[i].subtype <<
+             ", " << result[i].name << ", " << result[i].address << endl;
     }
     // tree.print();
 }
